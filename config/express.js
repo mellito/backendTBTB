9d48eab588f20490d959efe4b623a94f9b2b9884
mellito <dayanalexismanrique@hotmail.com>
2022-07-07 22:16:37 -0500

    take off cors

--- config/express.js
+++ config/express.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 
 function configExpress(app) {
   app.use(express.json());
-  app.use(cors());
 }
 
 module.exports = configExpress;
